3aacd3d7279caeac7b978dd3f9b034f9c8a24c8e
Joshua Valladares <joshuadvalladares@gmail.com>
2023-05-31 15:44:35 -0700

    so it begins

--- Driver.java
+++ Driver.java
@@ -1,7 +1,14 @@
 public class Driver{
 
-  public static void main(String[] args){
-    System.out.print("Hello World!\n");
+  public static void main(String[] args) throws Exception{
+    double[] data = {0,1,2,3,4,5};
+    Matrix a = Matrix.create(data,2,3);
+    Matrix b = Matrix.create(data,3,2);
+    a.print();
+    b.print();
+    Matrix.productClone(a,b).print();
+    Matrix.productClone(b,a).print();
+    System.out.print("Ain't that neat?\n");
     return;
   }
 }
